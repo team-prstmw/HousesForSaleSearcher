bbb0df5bffe8be69100d0f099ed0f4318b2e5bd2
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-01-14 00:55:43 +0100

    attach RegisterLoginModal to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
 import './App.css';
+import React from 'react';
 
-import { useState } from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+import RegisterLoginModal from './components/RegisterLoginModal/RegisterLoginModal';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#7ED6DF',
+      success: '#6ab04c',
+      error: '#eb4d4b',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#535c68',
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo.svg" className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((prev) => prev + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <RegisterLoginModal />
+      </div>
+    </ThemeProvider>
   );
 }
 
