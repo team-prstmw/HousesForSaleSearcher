6e79e5474c9005835bd51fb622b62a6919a6e239
Wojciech Okularczyk <wojciechokularczyk@gmail.com>
2022-01-16 08:17:37 +0100

    basic routing created

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,20 @@
 import './App.css';
 
-import { useState } from 'react';
+import { Route, Router, Switch } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo.svg" className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((prev) => prev + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/" component={IndexPage} />
+          <Route path="/user" component={UserPage} />
+          <Route path="/addhouse" component={AddHousePage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
