1b513ee4fef0e27ff411ea4e3c19799511440f8a
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-01-18 01:03:20 +0100

    add styles for RegisterLoginModal

--- src/components/RegisterLoginModal/RegisterLoginModal.jsx
+++ src/components/RegisterLoginModal/RegisterLoginModal.jsx
@@ -5,37 +5,39 @@ import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import RegisterLoginHeader from '../RegisterLoginHeader/RegisterLoginHeader';
 import RegisterForm from '../RegisterForm/RegisterForm';
+import LoginForm from '../LoginForm/LoginForm';
+
+import styles from './RegisterLoginModal.module.css';
 
 function RegisterLoginModal() {
   const [open, setOpen] = React.useState(false);
+  const [checked, setChecked] = React.useState(false);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
 
   return (
     <>
       <Button onClick={handleOpen}>Sign in</Button>
       <Modal
+        className={styles.registerLoginModal}
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-        sx={{ display: 'flex', justifyContent: 'center' }}
       >
         <Box
+          className={styles.registerLoginModal__wrapper}
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-            flexWrap: 'nowrap',
-            alignContent: 'center',
             bgcolor: 'background.paper',
-            width: 375,
-            height: 812,
           }}
         >
-          <RegisterLoginHeader />
-          <RegisterForm />
+          <RegisterLoginHeader checked={checked} onChange={handleChange} />
+          {checked ? <RegisterForm /> : <LoginForm />}
         </Box>
       </Modal>
     </>
