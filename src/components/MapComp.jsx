a09192a162fc1a4462c5b0aec80952af8230adf4
Wojciech Okularczyk <wojciech.okularczyk@coderscrew.pl>
2022-01-22 15:21:16 +0100

    Passed coordinates displays pins on the map

--- src/components/MapComp.jsx
+++ src/components/MapComp.jsx
@@ -1,9 +1,30 @@
-import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
+import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps';
 
-import API_KEY from '../googleMapsApiKey';
+import API_KEY from './mapComp/googleMapsApiKey';
 
+// values for testing
+const housesCoords = {
+  house1: { lat: 54.415924, lng: 18.566982, avalible: true, id: 1 },
+  house2: { lat: 54.410104, lng: 18.594983, avalible: true, id: 2 },
+  house3: { lat: 54.395131, lng: 18.601079, avalible: false, id: 3 },
+  house4: { lat: 54.389069, lng: 18.583625, avalible: true, id: 4 },
+};
 function Map() {
-  return <GoogleMap defaultZoom={12} defaultCenter={{ lat: 54.37, lng: 18.63 }} />;
+  return (
+    <GoogleMap defaultZoom={12} defaultCenter={{ lat: 54.37, lng: 18.63 }}>
+      {Object.values(housesCoords)
+        .filter((house) => house.avalible)
+        .map((house) => (
+          <Marker
+            key={house.id}
+            position={{
+              lat: house.lat,
+              lng: house.lng,
+            }}
+          />
+        ))}
+    </GoogleMap>
+  );
 }
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
