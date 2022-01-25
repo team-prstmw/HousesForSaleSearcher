b00d9f39c5c1cfdd20b80a9028347e86a4ecbfd9
mgoniszewska <marta.goniszewska@outlook.com>
2022-01-25 17:56:19 +0100

    Changes after cr

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
           <Routes>
             <Route path="/" element={<h1>INDEX PAGE</h1>} />
             <Route path="/user" element={<h1>USER PAGE</h1>} />
-            <Route path="/addhouse" element={<h1>ADD_HOUSE PAGE</h1>} />
             <Route path="/favorites" element={<h1>FAVORITES</h1>} />
             <Route path="/sell-house" element={<SellHousePage />} />
           </Routes>
