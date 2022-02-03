571846b507b83b55f807ad8f54ff1907d006c093
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-02-03 01:18:43 +0100

    add LoginProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { ThemeProvider } from '@mui/material/styles';
-import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import RegisterLoginModal from '/src/components/RegisterLoginModal/RegisterLoginModal';
 
 import HomePage from './pages/HomePage/HomePage';
 import SellHousePage from './pages/SellHousePage/SellHousePage';
 import theme from './theme/theme';
+import LoginProvider from './utils/auth/LoginProvider';
 
 function App() {
-  // const [LoggedIn, setLoggedIn] = React.useState(false);
   return (
     <BrowserRouter>
       <div className="App">
         <ThemeProvider theme={theme}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/user" element={<h1>USER PAGE</h1>} />
-            <Route path="/favorites" element={<h1>FAVORITES</h1>} />
-            <Route path="/sell-house" element={<SellHousePage />} />
-          </Routes>
+          <LoginProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/user" element={<h1>USER PAGE</h1>} />
+              <Route path="/favorites" element={<h1>FAVORITES</h1>} />
+              <Route path="/sell-house" element={<SellHousePage />} />
+            </Routes>
+          </LoginProvider>
         </ThemeProvider>
       </div>
     </BrowserRouter>
