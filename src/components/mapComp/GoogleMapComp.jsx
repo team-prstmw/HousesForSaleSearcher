b180f32e7fdba84e383330891539439b7f7c2b36
Wojciech Okularczyk <wojciech.okularczyk@coderscrew.pl>
2022-02-07 22:52:25 +0100

    synchronic version is working

--- src/components/mapComp/GoogleMapComp.jsx
+++ src/components/mapComp/GoogleMapComp.jsx
@@ -8,32 +8,38 @@ import getCoordsFromAddress from '../../utils/services/getCoordsFromAddress';
 const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 const HFSS_API = 'https://houses-for-sale-sandbox-default-rtdb.europe-west1.firebasedatabase.app/.json';
 
-// const housesCoords = {
-//   house1: { lat: 54.415924, lng: 18.566982, avalible: true, id: 1 },
-//   house2: { lat: 54.410104, lng: 18.594983, avalible: true, id: 2 },
-//   house3: { lat: 54.395131, lng: 18.601079, avalible: false, id: 3 },
-//   house4: { lat: 54.389069, lng: 18.583625, avalible: true, id: 4 },
-// };
+const housesCoords = [
+  [54.415924, 18.566982],
+  [54.410104, 18.594983],
+  [54.395131, 18.601079],
+  [54.389069, 18.583625],
+];
 
 function Map() {
   // eslint-disable-next-line no-undef
-  const geocoder = new google.maps.Geocoder();
+  // const geocoder = new google.maps.Geocoder();
 
-  const [housesCoords, setHousesCoords] = useState(null);
+  // const [housesCoords, setHousesCoords] = useState(null);
 
-  getCoordsFromAddress(getAddresses(HFSS_API), geocoder).then((data) => setHousesCoords(data));
+  // useEffect(() => {
+  //   if (housesCoords === null)
+  //     getCoordsFromAddress(getAddresses(HFSS_API), geocoder).then((data) => setHousesCoords(data));
+  // });
 
   console.log(housesCoords);
   return (
     <GoogleMap defaultZoom={7} defaultCenter={{ lat: 54.37, lng: 18.63 }}>
-      {housesCoords.map((house) => (
-        <Marker
-          position={{
-            lat: house[0],
-            lng: house[1],
-          }}
-        />
-      ))}
+      {housesCoords.map((coords) => {
+        console.log(coords, 'in map coords');
+        return (
+          <Marker
+            position={{
+              lat: coords[0],
+              lng: coords[1],
+            }}
+          />
+        );
+      })}
     </GoogleMap>
   );
 }
