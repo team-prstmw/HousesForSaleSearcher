ecbf0e4a90959c8afc5ddeac4d5f0769365fc059
Wojciech Okularczyk <wojciech.okularczyk@coderscrew.pl>
2022-02-08 19:36:52 +0100

    working Map component

--- src/components/mapComp/GoogleMapComp.jsx
+++ src/components/mapComp/GoogleMapComp.jsx
@@ -1,30 +1,33 @@
 import { useEffect, useState } from 'react';
 import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps';
 
-// import makeRequest from '../../utils/services/makeRequest';
+import { readAll } from '../../utils/api/read';
 import getAddresses from '../../utils/services/getAddresses';
 import getCoordsFromAddress from '../../utils/services/getCoordsFromAddress';
+// import makeRequest from '../../utils/services/makeRequest';
 
 const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 const HFSS_API = 'https://houses-for-sale-sandbox-default-rtdb.europe-west1.firebasedatabase.app/.json';
 
-const housesCoords = [
-  [54.415924, 18.566982],
-  [54.410104, 18.594983],
-  [54.395131, 18.601079],
-  [54.389069, 18.583625],
-];
+// const housesCoords = [
+//   [54.415924, 18.566982],
+//   [54.410104, 18.594983],
+//   [54.395131, 18.601079],
+//   [54.389069, 18.583625],
+// ];
 
 function Map() {
   // eslint-disable-next-line no-undef
-  // const geocoder = new google.maps.Geocoder();
+  const geocoder = new google.maps.Geocoder();
 
   // const [housesCoords, setHousesCoords] = useState(null);
+  const [housesCoords, setHousesCoords] = useState([]);
 
-  // useEffect(() => {
-  //   if (housesCoords === null)
-  //     getCoordsFromAddress(getAddresses(HFSS_API), geocoder).then((data) => setHousesCoords(data));
-  // });
+  useEffect(() => {
+    // if (housesCoords === null)
+    // getCoordsFromAddress(getAddresses(HFSS_API), geocoder).then((data) => setHousesCoords(data));
+    if (!housesCoords.length) getCoordsFromAddress(readAll('houses'), geocoder, setHousesCoords);
+  }, []);
 
   console.log(housesCoords);
   return (
--- src/utils/services/getCoordsFromAddress.js
+++ src/utils/services/getCoordsFromAddress.js
@@ -1,7 +1,8 @@
-export default async function getCoordsFromAddress(response, geocoder) {
+export default async function getCoordsFromAddress(response, geocoder, setHouseCoords) {
   const coordsList = [];
   const addressesList = await response;
-  addressesList.forEach((address) => {
+  addressesList.forEach((house, i) => {
+    const address = `${house.city} ${house.streetNumber} ${house.streetName}`;
     geocoder.geocode({ address }, (results, status) => {
       if (status === 'OK') {
         const lat = results[0].geometry.location.lat();
@@ -9,10 +10,13 @@ export default async function getCoordsFromAddress(response, geocoder) {
         // console.log(address, lat, lng);
         coordsList.push([lat, lng]);
       } else {
-        // console.log(`${address} ,Geocode error: ${status}`);
+        console.log(`${address} ,Geocode error: ${status}`);
+      }
+      if (i === addressesList.length - 1) {
+        console.log(coordsList);
+        setHouseCoords(coordsList);
       }
     });
   });
-  //   console.log(coordsList);
   return coordsList;
 }
