62911a6280581d16ccd8a4e7a50f68cca45ca288
Wojciech Okularczyk <wojciech.okularczyk@coderscrew.pl>
2022-02-08 20:12:55 +0100

    working Map component - it shows houses locations on the map

--- src/components/mapComp/GoogleMapComp.jsx
+++ src/components/mapComp/GoogleMapComp.jsx
@@ -2,38 +2,22 @@ import { useEffect, useState } from 'react';
 import { GoogleMap, Marker, withGoogleMap, withScriptjs } from 'react-google-maps';
 
 import { readAll } from '../../utils/api/read';
-import getAddresses from '../../utils/services/getAddresses';
 import getCoordsFromAddress from '../../utils/services/getCoordsFromAddress';
-// import makeRequest from '../../utils/services/makeRequest';
 
 const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
-const HFSS_API = 'https://houses-for-sale-sandbox-default-rtdb.europe-west1.firebasedatabase.app/.json';
-
-// const housesCoords = [
-//   [54.415924, 18.566982],
-//   [54.410104, 18.594983],
-//   [54.395131, 18.601079],
-//   [54.389069, 18.583625],
-// ];
 
 function Map() {
-  // eslint-disable-next-line no-undef
-  const geocoder = new google.maps.Geocoder();
-
-  // const [housesCoords, setHousesCoords] = useState(null);
   const [housesCoords, setHousesCoords] = useState([]);
 
   useEffect(() => {
-    // if (housesCoords === null)
-    // getCoordsFromAddress(getAddresses(HFSS_API), geocoder).then((data) => setHousesCoords(data));
+    // eslint-disable-next-line no-undef
+    const geocoder = new google.maps.Geocoder();
     if (!housesCoords.length) getCoordsFromAddress(readAll('houses'), geocoder, setHousesCoords);
-  }, []);
+  }, [housesCoords.length]);
 
-  console.log(housesCoords);
   return (
-    <GoogleMap defaultZoom={7} defaultCenter={{ lat: 54.37, lng: 18.63 }}>
+    <GoogleMap defaultZoom={6} defaultCenter={{ lat: 52.12, lng: 18.38 }}>
       {housesCoords.map((coords) => {
-        console.log(coords, 'in map coords');
         return (
           <Marker
             position={{
@@ -47,9 +31,8 @@ function Map() {
   );
 }
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
-
 export default function GoogleMapComp() {
+  const WrappedMap = withScriptjs(withGoogleMap(Map));
   return (
     <div style={{ width: '90%', height: '600px' }} sx={{}} className="google-map-container">
       <WrappedMap
